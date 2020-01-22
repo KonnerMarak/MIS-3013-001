61d377147ebf59f1fb5d2042199a60d7aadf43b5
Konner Marak <konnermarak@ou.edu>
2020-01-22 12:34:08 -0600

    Created in class examples solution and created a project for variables

---
+++ InClassExamples/Variables/Program.cs
@@ -0,0 +1,17 @@
+﻿//Konner Marak
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
