47ccebf437526ac54c80737d11ea7523b3ff1b17
Konner Marak <konnermarak@ou.edu>
2020-01-22 12:54:04 -0600

    CREATED VARIABLE FOR FIRSTNAME LAST NAME FAVORITE NUMBER

--- InClassExamples/Variables/Program.cs
+++ InClassExamples/Variables/Program.cs
@@ -12,6 +12,30 @@ namespace Variables
     {
         static void Main(string[] args)
         {
+            //create an string names firstName
+            string firstName;
+
+
+
+            //assign a value to first name
+            firstName = "Konner";
+
+
+            //Creat an int named favoriteNumber and assign your favorite number to it
+            int favoriteNumber = 13;
+
+            double crazyNumber = 7.767 * favoriteNumber;
+
+
+            string lastName = "Marak";
+            string fullName = firstName + " " + lastName;
+
+            Console.WriteLine(fullName + "has the favorite number of " + favoriteNumber + "\nwhich equates to a crazy number of " + crazyNumber);
+                // MAKE IT DYNAMIC
+        
+
+            Console.ReadKey();
+
         }
     }
 }
